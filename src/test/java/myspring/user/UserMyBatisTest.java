b5660fbdba9c78c5bec8dceef7b9943a284fec3d
MinJeong <106390284+Mineong@users.noreply.github.com>
2025-04-29 11:19:14 +0900

    함수형 인터페이스

--- src/test/java/myspring/user/UserMyBatisTest.java
+++ src/test/java/myspring/user/UserMyBatisTest.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,30 +25,48 @@ import myspring.user.vo.UserVO;
 @ContextConfiguration(locations = "classpath:spring-beans-mybatis.xml")
 public class UserMyBatisTest {
 	protected static final Logger logger = LogManager.getLogger();
-	
+
 	@Autowired
 	DataSource dataSource;
-	
+
 	@Autowired
 	SqlSessionFactory sessionFactory;
-	
+
 	@Autowired
 	SqlSession sqlSession;
 	
 	@Test
+	void userMapper() {
+		UserVO user = sqlSession.selectOne("userNS.selectUserById", "dooly");
+		logger.debug(user);
+	}
+
+	@Test @Disabled
 	void sqlSession() {
 		System.out.println(sessionFactory.getClass().getName());
 		UserVO user = sqlSession.selectOne("userNS.selectUserById", "dooly");
 		logger.debug(user);
-		
-		// Anonymous Inner Class (익명 클래스)
-		List<UserVO> userList = sqlSession.selectList("userNS.selectUserById"); // List<UserVO>
+
+		// Anonymous Inner Class (익명 내부 클래스)
+		List<UserVO> userList = sqlSession.selectList("userNS.selectUserList"); // List<UserVO>
+		// 기존의 For Loop
 		for (UserVO userVO : userList) {
 			logger.debug(userVO);
 		}
+		// .forEach(Consumer)에서 Consumer를 Anonymous Inner class 형태로 선언하는 방식
+		userList.forEach(new Consumer<UserVO>() {
+			@Override
+			public void accept(UserVO user) {
+				logger.debug(user);
+			}
+		});
+		// .forEach(Consumer)에서 Consumer를 Lambda Expression (람다식)으로 선언하는 방식
+		userList.forEach(user1 -> System.out.println(user1));
+		
+		// .forEach(Consumer)에서 Consumer를 Method Reference 로 선언하는 방식
+		userList.forEach(System.out::println);
 	}
-	
-	
+
 	@Test
 	void connection() {
 		try {
